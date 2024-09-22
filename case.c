c5a553edb5aaef4532d3414a4310b8d9534fa46a
Vincentcestmoi <vincent.wl.bruneau@gmail.com>
2024-09-23 00:32:10 +0200

    case.c -modification du create_maze (cellule inaccessible au centre, entré et sortie ouverte) -implémentation du print_maze

--- case.c
+++ case.c
@@ -43,10 +43,84 @@ cell **create_maze(const int width, const int height) {
             }
         }
     }
+    maze[width/2][height/2].wall_down = true;
+    maze[width/2][height/2].wall_up = true;
+    maze[width/2][height/2].wall_left = true;
+    maze[width/2][height/2].wall_right = true;
+
+    maze[0][0].wall_up = false;
+    maze[0][0].wall_left = false;
+    maze[width - 1][height - 1].wall_down = false;
+    maze[width - 1][height - 1].wall_right = false;
     return maze;
 }
 
 void print_maze(cell **maze, const int width, const int height)
 {
-    printf("Not ready yet\n");
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0) //initilisation de la SDL avec l'image et les events (comprends des malloc)
+    {
+        const char *error = SDL_GetError();
+        fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", error);
+        SDL_Quit();
+        return;
+    }
+    SDL_Window *fenetre = SDL_CreateWindow("maze", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width * 20, height * 20, SDL_WINDOW_SHOWN); //creation d'une fenetre
+    SDL_Renderer *renderer = SDL_CreateRenderer(fenetre, -1, SDL_RENDERER_ACCELERATED); //creation d'un renderer
+    if(renderer == NULL)
+    {
+        renderer = SDL_CreateRenderer(fenetre, -1, SDL_RENDERER_SOFTWARE);
+        if(renderer == NULL)
+        {
+            const char *error = SDL_GetError();
+            fprintf(stderr, "Erreur de creation du renderer : %s\n", error);
+            SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(fenetre);
+            SDL_Quit();
+            return;
+        }
+    }
+    SDL_Delay(1000); //pause de 1 secondes
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); //on définit la couleur de fond en blanc
+
+    for(int x = 0; x < width; x++)
+    {
+        for(int y = 0; y < height; y++)
+        {
+            if(maze[y][x].wall_up)
+            {
+                SDL_RenderDrawLine(renderer, x * 20, y * 20, (x * 20) + 19, y * 20);
+            }
+            if(maze[y][x].wall_down)
+            {
+                SDL_RenderDrawLine(renderer, x * 20, (y * 20) + 19, (x * 20) + 19, (y * 20) + 19);
+            }
+            if(maze[y][x].wall_left)
+            {
+                SDL_RenderDrawLine(renderer, x * 20, y * 20, x * 20, (y * 20) + 19);
+            }
+            if(maze[y][x].wall_right)
+            {
+                SDL_RenderDrawLine(renderer, (x * 20) + 19, y * 20, (x * 20) + 19, (y * 20) + 19);
+            }
+        }
+    }
+
+    SDL_RenderPresent(renderer); //on met à jour l'affichage
+    SDL_Delay(500); //pause de 0.5 secondes
+    SDL_SetRenderDrawColor(renderer, 0, 255, 100, 255); //on définit la couleur en vert
+    SDL_RenderDrawLine(renderer, 0, 0, 0, 20); //l'entrée en vert
+    SDL_RenderDrawLine(renderer, 0, 0, 20, 0); //l'entrée en vert
+
+    SDL_RenderPresent(renderer); //on met à jour l'affichage
+    SDL_Delay(500); //pause de 0.5 secondes
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); //on définit la couleur en bleu
+    SDL_RenderDrawLine(renderer, (width * 20) - 1, (height * 20) - 20, (width * 20) - 1, (height * 20)); //la sortie en bleu
+    SDL_RenderDrawLine(renderer, (width * 20) - 20, (height * 20) - 1, (width * 20), (height * 20) - 1); //la sortie en bleu
+
+    SDL_RenderPresent(renderer); //on met à jour l'affichage
+    SDL_Delay(5000); //pause de 5 secondes
+
+    SDL_DestroyRenderer(renderer); //destruction du renderer (desallocation de la memoire)
+    SDL_DestroyWindow(fenetre); //destruction de la fenetre (desallocation de la memoire)
+    SDL_Quit(); //desalocation de la memoire
 }
