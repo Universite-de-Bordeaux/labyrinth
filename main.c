e96d6788ac2f678f72f2a36e37cca6f140b2b93c
Vincentcestmoi <vincent.wl.bruneau@gmail.com>
2024-09-23 14:17:04 +0200

    main -test

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include "case.h"
+#include "outside.h"
 
 int main() {
-    const int width = 5;
-    const int height = 5;
+    const int width = 25;
+    const int height = 25;
     cell **test = create_basic_maze(width, height);
+    wall_up(test, 10, 10, width, height);
+    wall_down(test, 10, 10, width, height);
+    wall_right(test, 10, 10, width, height);
     if(print_maze(test, width, height) == -1)
     {
         return 1;
