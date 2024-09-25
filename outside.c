2f6a7a8cdbb968f4b1bbce329d75e5bbbc3d84bf
Vincentcestmoi <vincent.wl.bruneau@gmail.com>
2024-09-25 13:57:58 +0200

    outside.c -implémentation fonction maze_from_file

--- outside.c
+++ outside.c
@@ -16,6 +16,26 @@ maze_t maze_from_file(const char *filename) {
     printf("height = %d\n", height); //pour le debbug au cas où
     maze_t maze = create_basic_maze(width, height); //créer un labyrinthe de base
 
+    char *line2;
+    line2 = line;
+    int x, y;
+    char *wall;
+    fgets(line, 6, file);
+    while(line2 != line)
+    {
+    	x = atoi(strtok(line, " "));
+    	y = atoi(strtok(NULL, " "));
+    	wall = strtok(NULL, " ");
+    	if(strcmp(wall, "h") == 0)
+     	{
+      		wall_down(maze, x, y);
+      	}
+    	if(strcmp(wall, "v") == 0)
+      	{
+      		wall_right(maze, x, y);
+      	}
+    }
+
     fclose(file); //désallouer le fichier
     return maze;
 }
