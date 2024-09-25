721d70d037f7d678de18595a64e3803f3bf3c552
Vincentcestmoi <vincent.wl.bruneau@gmail.com>
2024-09-25 21:14:51 +0200

    main.c -sécurité

--- main.c
+++ main.c
@@ -6,12 +6,14 @@ int main() {
     maze_t test = create_basic_maze(width, height);
     if(print_maze(test) == -1)
     {
+        free_maze(test);
         return 1;
     }
     free_maze(test);
     test = maze_from_file("test");
     if(print_maze(test) == -1)
     {
+        free_maze(test);
         return 1;
     }
     free_maze(test);
