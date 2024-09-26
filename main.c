6284dda2c9bc65ad38abde0b2cd4ab5e06f941f2
Vincentcestmoi <vincent.wl.bruneau@gmail.com>
2024-09-26 23:41:39 +0200

    main.c -import -tests

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 #include "outside.h"
+#include "mazemaker.h"
 
 int main() {
-    const maze_t test = maze_from_file("test");
+    maze_t test = maze_from_file("test");
     if(print_maze(test) == -1)
     {
         free_maze(test);
         return 1;
     }
     free_maze(test);
+    test = line_maze(10, 10);
+    if(print_maze(test) == -1)
+    {
+        free_maze(test);
+        return 1;
+    }
     return 0;
 }
