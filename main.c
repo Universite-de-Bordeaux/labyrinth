d6809599d9c8e0d0fd11a618f0476262f9940a01
Vincentcestmoi <vincent.wl.bruneau@gmail.com>
2024-09-27 00:25:44 +0200

    main.c -tests -adaptation aux modifications

--- main.c
+++ main.c
@@ -3,17 +3,25 @@
 
 int main() {
     maze_t test = maze_from_file("test");
-    if(print_maze(test) == -1)
+    if(print_maze(test, "lecture") == -1)
     {
         free_maze(test);
         return 1;
     }
     free_maze(test);
     test = line_maze(10, 10);
-    if(print_maze(test) == -1)
+    if(print_maze(test, "line_maze") == -1)
     {
         free_maze(test);
         return 1;
     }
+    free_maze(test);
+    test = column_maze(10, 10);
+    if(print_maze(test, "column_maze") == -1)
+    {
+        free_maze(test);
+        return 1;
+    }
+    free_maze(test);
     return 0;
 }
