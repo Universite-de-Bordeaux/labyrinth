c70ddf5c9c6dfc1a528b08ddb8031d8cdd671b19
Vincentcestmoi <vincent.wl.bruneau@gmail.com>
2024-09-26 23:43:27 +0200

    mazemaker.c -implémentation line_maze

--- mazemaker.c
+++ mazemaker.c
@@ -1,5 +1,25 @@
-//
-// Created by vins on 26/09/24.
-//
-
 #include "mazemaker.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+//Crée un labyrinthe parfait de taille width x height
+//tous les murs sont verticaux
+maze_t line_maze(const int width, const int height)
+{
+    const time_t t = time(NULL);
+    srand(t);
+    const maze_t maze = create_basic_maze(width, height);
+    for(int i = 0; i < width - 1; i++)
+    {
+        const int break_wall = rand() % height;
+        for(int j = 0; j < height; j++)
+        {
+            if(j != break_wall)
+            {
+                wall_right(maze, i, j);
+            }
+        }
+    }
+    return maze;
+}
