9171a5f5744775db515979d109001321e0b4f7e6
Vincentcestmoi <vincent.wl.bruneau@gmail.com>
2024-09-26 23:13:00 +0200

    main.c -test

--- main.c
+++ main.c
@@ -1,16 +1,7 @@
 #include "outside.h"
 
 int main() {
-    const int width = 25;
-    const int height = 25;
-    maze_t test = create_basic_maze(width, height);
-    if(print_maze(test) == -1)
-    {
-        free_maze(test);
-        return 1;
-    }
-    free_maze(test);
-    test = maze_from_file("test");
+    const maze_t test = maze_from_file("test");
     if(print_maze(test) == -1)
     {
         free_maze(test);
