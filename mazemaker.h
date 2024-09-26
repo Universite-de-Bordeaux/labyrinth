28afece86aeafdd674b38db7f5c4297ab4b32233
Vincentcestmoi <vincent.wl.bruneau@gmail.com>
2024-09-26 23:42:23 +0200

    mazemaker.h -déclaration line_maze -déclaration one_way_maze

--- mazemaker.h
+++ mazemaker.h
@@ -1,8 +1,8 @@
-//
-// Created by vins on 26/09/24.
-//
-
 #ifndef MAZEMAKER_H
 #define MAZEMAKER_H
+#include "case.h"
+
+maze_t line_maze(const int width, const int height);
+maze_t one_way_maze(const int width, const int height);
 
 #endif //MAZEMAKER_H
