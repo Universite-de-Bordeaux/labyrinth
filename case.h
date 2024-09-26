bb37e226499940ece5c98aabe2802c4aa511c605
Vincentcestmoi <vincent.wl.bruneau@gmail.com>
2024-09-27 00:24:52 +0200

    case.h -ajout docstrings

--- case.h
+++ case.h
@@ -9,19 +9,26 @@ struct cell {
     bool wall_left;
     bool wall_right;
 };
-
 struct maze {
     int width;
     int height;
     struct cell **cells;
 };
 
+//Structure représentant une cellule
+//contient 4 booléens représentant les murs de la cellule :
+//wall_up, wall_down, wall_left, wall_right
 typedef struct cell cell;
+//Structure représentant un labyrinthe
+//width : largeur du labyrinthe
+//height : hauteur du labyrinthe
+//cells : tableau de tableau de cellules
+//malloc utilisés, penser à free_maze pour libérer la mémoire
 typedef struct maze maze_t;
 
 maze_t create_basic_maze(int width, int height);
 void free_maze(maze_t maze);
-int print_maze(maze_t maze);
+int print_maze(maze_t maze, const char* filename);
 void wall_up(maze_t maze, int x, int y);
 void wall_down(maze_t maze, int x, int y);
 void wall_left(maze_t maze, int x, int y);
