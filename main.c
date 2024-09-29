efe458c1121b3ab7694a027b062db203e6c8a8bb
Vincentcestmoi <vincent.wl.bruneau@gmail.com>
2024-09-29 20:16:06 +0200

    main.c -tests

--- main.c
+++ main.c
@@ -3,13 +3,6 @@
 #include "solveur.h"
 
 int main() {
-    const maze_t test = box_maze(10, 10);
-if(print_maze(test, "test") != 0)
-    {
-        free_maze(test);
-        return -1;
-    }
-    free_maze(test);
-
+    rminigame3(3, 3);
     return 0;
 }
