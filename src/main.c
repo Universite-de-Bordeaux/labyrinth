6c56abe965ce8d6a64b77bc7ca586b7b23e7382d
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-10-07 19:49:44 +0200

    main.c -tests

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int main() {
-  const maze_t maze = rmaze(10, 10);
-  cascade(maze) ? printf("solvable\n") : printf("insolvable\n");
-  return 0;
+  const maze_t maze = hunt_kill_maze(50, 50);
+  is_exit_cascade(maze) ? printf("Exit found\n") : printf("Exit not found\n");
+  return print_maze(maze, "maze") ? 0 : 1;
 }
