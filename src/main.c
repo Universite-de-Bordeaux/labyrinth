e2414aaecca80d5e2054b433608e9947661e4c8c
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-10-10 21:48:27 +0200

    main.c -test

--- src/main.c
+++ src/main.c
@@ -5,9 +5,9 @@
 
 int main() {
   const maze_t maze = rmaze(95, 50);
-  //wall_up(maze, 94, 49);
-  //wall_left(maze, 94, 49);
-  show_fast_has_exit_cascade(maze);
+  wall_up(maze, 94, 49);
+  wall_left(maze, 94, 49);
+  show_has_exit_cascade(maze);
   free_maze(maze);
   return 0;
 }
