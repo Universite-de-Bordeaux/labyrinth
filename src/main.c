5f8837bcd9e6a9489a168447d4e7703f81d987c0
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-10-10 21:27:28 +0200

    main.c -test

--- src/main.c
+++ src/main.c
@@ -4,7 +4,9 @@
 #include <stdio.h>
 
 int main() {
-  const maze_t maze = rmaze(25, 25);
+  const maze_t maze = imperfect_one_way_maze(95, 50);
+  wall_up(maze, 94, 49);
+  wall_left(maze, 94, 49);
   show_has_exit_cascade(maze);
   free_maze(maze);
   return 0;
