0ceff42b36fe55ff2883e6e0359333beaa47de02
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-10-11 12:56:00 +0200

    main.c -tests

--- src/main.c
+++ src/main.c
@@ -4,12 +4,9 @@
 #include <stdio.h>
 
 int main() {
-  const maze_t maze = rperfect_maze(95, 50);
-  wall_up(maze, 94, 49);
-  wall_left(maze, 94, 49);
-  unwall_up(maze, 20, 20);
-  unwall_left(maze, 20, 20);
-  unwall_down(maze, 20, 20);
+  const maze_t maze = rmaze(80, 50);
+  wall_up(maze, 79, 49);
+  wall_left(maze, 79, 49);
   show_is_perfect_cascade(maze);
   free_maze(maze);
   return 0;
