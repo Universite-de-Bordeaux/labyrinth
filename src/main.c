b12e5fadfacae2def2548ee2b12f8c8c028e5671
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-10-12 19:31:57 +0200

    main.c -tests -gestion import

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
 #include "mazemaker.h"
+#include "solveur.h"
 #include "test.h"
 
 int main() {
-    EVALUATE_MAZEMAKERN(perfect_one_way_maze, "perfect_one_way_maze");
-    EVALUATE_MAZEMAKERN(imperfect_one_way_maze, "imperfect_one_way_maze");
-    EVALUATE_MAZEMAKER(line_maze);
+    EVALUATE_MAZEMAKERN(perfect_one_way_maze, "perfect_one_way_maze");        //théoriquement parfait
+    EVALUATE_MAZEMAKERN(imperfect_one_way_maze, "imperfect_one_way_maze");    //théoriquement solvable
+    EVALUATE_MAZEMAKERN(hunt_kill_maze, "hunt_kill_maze");                    //théoriquement parfait
+    EVALUATE_MAZEMAKERN(by_path_maze, "by_path_maze");                        //théoriquement parfait
+    EVALUATE_MAZEMAKERN(line_maze, "line_maze");                              //théoriquement parfait
+    EVALUATE_MAZEMAKERN(column_maze, "column_maze");                          //théoriquement parfait
+    EVALUATE_MAZEMAKERN(create_basic_maze, "create_basic_maze");              //théoriquement solvable
+    EVALUATE_MAZEMAKERN(create_wall_maze, "create_wall_maze");                //théoriquement insolvable
     return 0;
 }
