88c5ee855c50b50c3899f5df493c5f26daa0cbd6
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-10-12 20:54:49 +0200

    main.c -test temp de génération des labyrinthes

--- src/main.c
+++ src/main.c
@@ -1,15 +1,38 @@
 #include "mazemaker.h"
 #include "solveur.h"
 #include "test.h"
+#include <time.h>
 
 int main() {
-    EVALUATE_MAZEMAKERN(perfect_one_way_maze, "perfect_one_way_maze");        //théoriquement parfait
-    EVALUATE_MAZEMAKERN(imperfect_one_way_maze, "imperfect_one_way_maze");    //théoriquement solvable
-    EVALUATE_MAZEMAKERN(hunt_kill_maze, "hunt_kill_maze");                    //théoriquement parfait
-    EVALUATE_MAZEMAKERN(by_path_maze, "by_path_maze");                        //théoriquement parfait
-    EVALUATE_MAZEMAKERN(line_maze, "line_maze");                              //théoriquement parfait
-    EVALUATE_MAZEMAKERN(column_maze, "column_maze");                          //théoriquement parfait
-    EVALUATE_MAZEMAKERN(create_basic_maze, "create_basic_maze");              //théoriquement solvable
-    EVALUATE_MAZEMAKERN(create_wall_maze, "create_wall_maze");                //théoriquement insolvable
+    //EVALUATE_MAZEMAKERN(perfect_one_way_maze, "perfect_one_way_maze");        //théoriquement parfait
+    //EVALUATE_MAZEMAKERN(imperfect_one_way_maze, "imperfect_one_way_maze");    //théoriquement solvable
+    //EVALUATE_MAZEMAKERN(hunt_kill_maze, "hunt_kill_maze");                    //théoriquement parfait
+    //EVALUATE_MAZEMAKERN(by_path_maze, "by_path_maze");                        //théoriquement parfait
+    //EVALUATE_MAZEMAKERN(line_maze, "line_maze");                              //théoriquement parfait
+    //EVALUATE_MAZEMAKERN(column_maze, "column_maze");                          //théoriquement parfait
+    //EVALUATE_MAZEMAKERN(create_basic_maze, "create_basic_maze");              //théoriquement solvable
+    //EVALUATE_MAZEMAKERN(create_wall_maze, "create_wall_maze");                //théoriquement insolvable
+
+    //technique de la flemme
+    #define EVALUATE_TIME(générateur, name, x, y) do { \
+        time_t t = time(NULL); \
+        for(int i = 1; i < x; i++) \
+        { \
+            for(int j = 1; j < y; j++) \
+            { \
+                const maze_t maze = générateur(i, j); \
+                free_maze(maze); \
+            } \
+        } \
+        printf("Temps de génération par %s : \t%li secondes\n", name, time(NULL) - t); \
+    } while(0)
+    EVALUATE_TIME(create_basic_maze, "create_basic_maze", 100, 100);
+    EVALUATE_TIME(create_wall_maze, "create_wall_maze", 100, 100);
+    EVALUATE_TIME(line_maze, "line_maze", 100, 100);
+    EVALUATE_TIME(column_maze, "column_maze", 100, 100);
+    EVALUATE_TIME(imperfect_one_way_maze, "imperfect_one_way_maze", 100, 100);
+    EVALUATE_TIME(perfect_one_way_maze, "perfect_one_way_maze", 100, 100);
+    EVALUATE_TIME(hunt_kill_maze, "hunt_kill_maze", 100, 100);
+    EVALUATE_TIME(by_path_maze, "by_path_maze", 100, 100);
     return 0;
 }
