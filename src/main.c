b7d2894183534f6f1802dfaaacac029ef8e5406a
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-10-14 09:38:25 +0200

    main.c -implémentation pour cmd

--- src/main.c
+++ src/main.c
@@ -2,69 +2,149 @@
 #include "test.h"
 #include <time.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "outside.h"
-#include "solveur.h"
+#include <string.h>
 
-int main() {
-    //EVALUATE_MAZEMAKERN(perfect_one_way_maze, "perfect_one_way_maze");        //théoriquement parfait
-    //EVALUATE_MAZEMAKERN(imperfect_one_way_maze, "imperfect_one_way_maze");    //théoriquement solvable
-    //EVALUATE_MAZEMAKERN(hunt_kill_maze, "hunt_kill_maze");                    //théoriquement parfait
-    //EVALUATE_MAZEMAKERN(by_path_maze, "by_path_maze");                        //théoriquement parfait
-    //EVALUATE_MAZEMAKERN(line_maze, "line_maze");                              //théoriquement parfait
-    //EVALUATE_MAZEMAKERN(column_maze, "column_maze");                          //théoriquement parfait
-    //EVALUATE_MAZEMAKERN(create_basic_maze, "create_basic_maze");              //théoriquement solvable
-    //EVALUATE_MAZEMAKERN(create_wall_maze, "create_wall_maze");                //théoriquement insolvable
-
-    //EVALUATE_TIME(create_basic_maze, "create_basic_maze", 100, 100);          //très rapide
-    //EVALUATE_TIME(create_wall_maze, "create_wall_maze", 100, 100);            //très rapide
-    //EVALUATE_TIME(line_maze, "line_maze", 100, 100);                          //très rapide
-    //EVALUATE_TIME(column_maze, "column_maze", 100, 100);                      //très rapide
-    //EVALUATE_TIME(imperfect_one_way_maze, "imperfect_one_way_maze", 100, 100);//plutot rapide
-    //EVALUATE_TIME(perfect_one_way_maze, "perfect_one_way_maze", 100, 100);    //convenable
-    //EVALUATE_TIME(hunt_kill_maze, "hunt_kill_maze", 100, 100);                //WTF c'est SUPER LENT !!!
-    //EVALUATE_TIME(by_path_maze, "by_path_maze", 100, 100);                    //un peu lent
-
-    //const maze_t maze = column_maze(20, 20);
-    //for(int i = 0; i < 10; i++)
-    //{
-    //    unwall_down(maze, 19, i);
-    //}
-    //show_shortest_exit_right_hand(maze);
-    //free_maze(maze);
-    for(int k = 1; k < 100; k++)
+int main(const int argc, char *argv[]) {
+    if(argv[1] == NULL)
     {
-        for(int l = 1; l < 100; l++){
-            const maze_t maze = r_maze(k, l);
-            maze_to_file(maze, "maze");
-            const maze_t maze2 = maze_from_file("maze");
-            for(int i = 0; i < k; i++)
-            {
-                for(int j = 0; j < l; j++)
-                {
-                    if(has_wall_up(maze, i, j) != has_wall_up(maze2, i, j))
-                    {
-                        fprintf(stderr, "Erreur de copie de mur en haut en %d %d\n", i, j);
-                        return 0;
-                    }
-                    if(has_wall_down(maze, i, j) != has_wall_down(maze2, i, j))
-                    {
-                        fprintf(stderr, "Erreur de copie de mur en bas en %d %d\n", i, j);
-                        return 0;
-                    }
-                    if(has_wall_left(maze, i, j) != has_wall_left(maze2, i, j))
-                    {
-                        fprintf(stderr, "Erreur de copie de mur à gauche en %d %d\n", i, j);
-                        return 0;
-                    }
-                    if(has_wall_right(maze, i, j) != has_wall_right(maze2, i, j))
-                    {
-                        fprintf(stderr, "Erreur de copie de mur à droite en %d %d\n", i, j);
-                        return 0;
-                    }
-                }
-            }
+        printf("Usage : %s arg1 arg2 ... \n", argv[0]);
+        return 1;
+    }
+    if(!strcmp(argv[1], "-g") || !strcmp(argv[1], "--generate"))
+    {
+        if(argv[2] == NULL || argv[3] == NULL || argv[4] == NULL || argv[5] == NULL)
+        {
+            printf("Usage : %s -g <generator> <width> <height> <namefile>\n", argv[0]);
+            return 1;
+        }
+        maze_t maze;
+        if(!strcmp(argv[2], "powm"))
+        {
+            maze = perfect_one_way_maze(atoi(argv[3]), atoi(argv[4]));
+        }
+        else if(!strcmp(argv[2], "iowm"))
+        {
+            maze = imperfect_one_way_maze(atoi(argv[3]), atoi(argv[4]));
+        }
+        else if(!strcmp(argv[2], "hkm"))
+        {
+            maze = hunt_kill_maze(atoi(argv[3]), atoi(argv[4]));
+        }
+        else if(!strcmp(argv[2], "bpm"))
+        {
+            maze = by_path_maze(atoi(argv[3]), atoi(argv[4]));
+        }
+        else if(!strcmp(argv[2], "lm"))
+        {
+            maze = line_maze(atoi(argv[3]), atoi(argv[4]));
+        }
+        else if(!strcmp(argv[2], "cm"))
+        {
+            maze = column_maze(atoi(argv[3]), atoi(argv[4]));
+        }
+        else
+        {
+            printf("Usage : %s -g <generator> <width> <height> <namefile>\n", argv[0]);
+            printf("<generator> : powm, iowm, hkm, bpm, lm, cm\n");
+            return 1;
+        }
+        if(argv[5] == NULL)
+        {
+            printf("Usage : %s -g <generator> <width> <height> <namefile>\n", argv[0]);
+            return 1;
+        }
+        maze_to_file(maze, argv[5]);
+        printf("Maze saved in %s\n", argv[5]);
+        free_maze(maze);
+        return 0;
+    }
+    if(!strcmp(argv[1], "-e") || !strcmp(argv[1], "--evaluate"))
+    {
+        if(argv[2] == NULL || argv[3] == NULL)
+        {
+            printf("Usage : %s -e <generator> <type eval>\n", argv[0]);
+            return 1;
+        }
+        func_ptr f;
+        char *name;
+        if(!strcmp(argv[2], "powm"))
+        {
+            f = &perfect_one_way_maze;
+            name = "perfect_one_way_maze";
+        }
+        else if(!strcmp(argv[2], "iowm"))
+        {
+            f = &imperfect_one_way_maze;
+            name = "imperfect_one_way_maze";
+        }
+        else if(!strcmp(argv[2], "hkm"))
+        {
+            f = &hunt_kill_maze;
+            name = "hunt_kill_maze";
+        }
+        else if(!strcmp(argv[2], "bpm"))
+        {
+            f = &by_path_maze;
+            name = "by_path_maze";
+        }
+        else if(!strcmp(argv[2], "lm"))
+        {
+            f = &line_maze;
+            name = "line_maze";
+        }
+        else if(!strcmp(argv[2], "cm"))
+        {
+            f = &column_maze;
+            name = "column_maze";
+        }
+        else
+        {
+            printf("Usage : %s -e <generator> <type eval>\n", argv[0]);
+            printf("<generator> : powm, iowm, hkm, bpm, lm, cm\n");
+            return 1;
+        }
+        if(!strcmp(argv[3], "time"))
+        {
+            EVALUATE_TIME(f, name, 100, 100);
+        }
+        else if(!strcmp(argv[3], "type"))
+        {
+            EVALUATE_MAZEMAKER(f);
+        }
+        else
+        {
+            printf("Usage : %s -e <generator> <type eval>\n", argv[0]);
+            printf("<generator> : powm, iowm, hkm, bpm, lm, cm\n");
+            printf("<type eval> : time, type\n");
+            return 1;
         }
     }
-    printf("Looks good\n");    return 0;
+    if(!strcmp(argv[1], "-s") || !strcmp(argv[1], "--show"))
+    {
+        if(argv[2] == NULL)
+        {
+            printf("Usage : %s -s <filename>\n", argv[0]);
+            return 1;
+        }
+        const maze_t maze = maze_from_file(argv[2]);
+        print_maze(maze);
+        free_maze(maze);
+        return 0;
+    }
+    if(!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help"))
+    {
+        printf("<generator> : powm, iowm, hkm, bpm, lm, cm\n");
+        printf("<type eval> : time, type\n");
+        printf("Usage : %s -g <generator> <width> <height> <namefile>\n", argv[0]);
+        printf("Usage : %s -s <namefile>\n", argv[0]);
+        printf("Usage : %s -e <generator> <type eval>\n", argv[0]);
+        printf("Usage : %s -h\n", argv[0]);
+    }
+    else
+    {
+        printf("Usage : %s arg1 arg2 ... \n", argv[0]);
+    }
+    return 0;
 }
