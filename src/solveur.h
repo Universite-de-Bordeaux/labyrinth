a8781faac087edc2dfe2ef796de901200fd59422
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-10-25 21:41:22 +0200

    solveur.h -TODO : tout refaire (en optimisé)
    Signed-off-by: Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>

--- src/solveur.h
+++ src/solveur.h
@@ -6,16 +6,16 @@
 
 //renvoie true si le labyrinthe est solvable, false sinon
 //maze : le labyrinthe
-bool has_exit_right_hand(maze_t maze);
+bool has_exit_right_hand(maze_t maze); //TODO : réecrire à l'aide de piles
 
 //renvoie true si le labyrinthe est parfait, false sinon
 //maze : le labyrinthe
-bool is_perfect_right_hand(maze_t maze);
+bool is_perfect_right_hand(maze_t maze); //TODO : réecrire à l'aide de piles
 
 //renvoie le chemin le plus court pour sortir du labyrinthe
 //maze : le labyrinthe
 //renvoie un chemin vide si le labyrinthe n'est pas solvable
-way *shortest_exit_right_hand(const maze_t maze);
+way *shortest_exit_right_hand(const maze_t maze); //TODO : réecrire à l'aide de piles
 
 
 // --- visualisation ---
@@ -24,24 +24,24 @@ way *shortest_exit_right_hand(const maze_t maze);
 //affiche le labyrinthe et la progression du solveur
 //renvoie -1 en cas d'erreur, 1 sinon
 //maze : le labyrinthe
-int show_has_exit_right_hand(maze_t maze);
+int show_has_exit_right_hand(maze_t maze); //TODO
 
 //fonction de visualisation
 //affiche le labyrinthe et la progression du solveur
 //renvoie -1 en cas d'erreur, 1 sinon
 //maze : le labyrinthe
-int show_is_perfect_right_hand(maze_t maze);
+int show_is_perfect_right_hand(maze_t maze); //TODO
 
 //fonction de visualisation
 //affiche le labyrinthe et la progression du solveur
 //renvoie -1 en cas d'erreur, 1 sinon
 //maze : le labyrinthe
-int show_shortest_exit_right_hand(maze_t maze); //TODO : en cours
+int show_shortest_exit_right_hand(maze_t maze); //TODO
 
 //fonction de visualisation
 //affiche le chemin dans le labyrinthe
 //maze : le labyrinthe
 //w : le chemin
 //ne gère pas les cas d'erreur, ni les murs
-int show_the_way(const maze_t maze, const way *w);
+int show_the_way(const maze_t maze, const way *w); //TODO
 #endif //SOLVEUR_H
