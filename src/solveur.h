f984045619c52ca36302e4a9313aea5b8de272c1
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-10-25 20:53:42 +0200

    solveur.c solveur.h -suppression minigames
    Signed-off-by: Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>

--- src/solveur.h
+++ src/solveur.h
@@ -2,69 +2,7 @@
 #define SOLVEUR_H
 #include "struct.h"
 
-// --- mini-jeux ---
-
-//une mini-jeu où il faut trouver la sortie
-//version de base
-//le chemin apparaît au fur et à mesure que le joueur avance
-//maze : le labyrinthe
-//appuier sur ECHAP ou la croix fermera proprement le jeu
-//appuier sur ENTER abandonnera le jeu et affichera le labyrinthe en entier
-int minigame1(maze_t maze);
-
-//lance minigame1 avec un générateur de labyrinthe aléatoire
-//width : largeur du labyrinthe
-//height : hauteur du labyrinthe
-int rminigame1(int width, int height);
-
-//une mini-jeu où il faut trouver la sortie
-//le chemin n'apparaît que sur la case où le joueur est
-//version mi-blind
-//maze : le labyrinthe
-//appuier sur ECHAP ou la croix fermera proprement le jeu
-//appuier sur ENTER abandonnera le jeu et affichera le labyrinthe en entier
-int minigame2(maze_t maze);
-
-//lance minigame2 avec un générateur de labyrinthe aléatoire
-//width : largeur du labyrinthe
-//height : hauteur du labyrinthe
-//appuier sur ECHAP ou la croix fermera proprement le jeu
-//appuier sur ENTER abandonnera le jeu et affichera le labyrinthe en entier
-int rminigame2(int width, int height);
-
-//une mini-jeu où il faut trouver la sortie
-//le chemin n'apparaît pas, même sur la case où le joueur est
-//version full-blind
-//maze : le labyrinthe
-int minigame3(maze_t maze);
-
-//lance minigame3 avec un générateur de labyrinthe aléatoire
-//width : largeur du labyrinthe
-//height : hauteur du labyrinthe
-int rminigame3(int width, int height);
-
-//lance un mini-jeu où il faut trouver la sortie
-//seul la case où le joueur est est affichée
-//le reste du labyrinthe est caché
-//maze : le labyrinthe
-int minigame4(maze_t maze);
-
-//lance minigame4 avec un générateur de labyrinthe aléatoire
-//width : largeur du labyrinthe
-//height : hauteur du labyrinthe
-int rminigame4(int width, int height);
-
-//lance un mini-jeu avec un générateur de labyrinthe et une difficulté aléatoires
-//width : largeur du labyrinthe
-//height : hauteur du labyrinthe
-int rminigame(int width, int height);
-
-//lance un mini-jeu avec un labyrinthe donné et une difficulté aléatoire
-//maze : le labyrinthe
-int r_minigame(maze_t maze);
-
-// --- solveur left hand (parcours en profondeur) ---
-
+// --- solveurs ---
 
 //renvoie true si le labyrinthe est solvable, false sinon
 //maze : le labyrinthe
