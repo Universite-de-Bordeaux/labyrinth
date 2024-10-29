242aae242e1011e43adda12d647032c0bd6a1d58
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-10-29 19:18:21 +0100

    solveur.h -rename (nom plus parlant/honnête)
    Signed-off-by: Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>

--- src/solveur.h
+++ src/solveur.h
@@ -6,16 +6,16 @@
 
 //renvoie true si le labyrinthe est solvable, false sinon
 //maze : le labyrinthe
-bool has_exit_right_hand(maze_t maze); //TODO : réecrire à l'aide de piles
+bool exit_deep_seeker(maze_t maze); //TODO : réecrire à l'aide de piles
 
 //renvoie true si le labyrinthe est parfait, false sinon
 //maze : le labyrinthe
-bool is_perfect_right_hand(maze_t maze); //TODO : réecrire à l'aide de piles
+bool perfect_deep_inspector(maze_t maze); //TODO : réecrire à l'aide de piles
 
 //renvoie le chemin le plus court pour sortir du labyrinthe
 //maze : le labyrinthe
 //renvoie un chemin vide si le labyrinthe n'est pas solvable
-way *shortest_exit_right_hand(const maze_t maze); //TODO : réecrire à l'aide de piles
+way *best_exit_deep_seeker(const maze_t maze); //TODO : réecrire à l'aide de piles
 
 
 // --- visualisation ---
@@ -24,19 +24,19 @@ way *shortest_exit_right_hand(const maze_t maze); //TODO : réecrire à l'aide d
 //affiche le labyrinthe et la progression du solveur
 //renvoie -1 en cas d'erreur, 1 sinon
 //maze : le labyrinthe
-int show_has_exit_right_hand(maze_t maze); //TODO
+int show_exit_deep_seeker(maze_t maze); //TODO
 
 //fonction de visualisation
 //affiche le labyrinthe et la progression du solveur
 //renvoie -1 en cas d'erreur, 1 sinon
 //maze : le labyrinthe
-int show_is_perfect_right_hand(maze_t maze); //TODO
+int show_perfect_deep_inspector(maze_t maze); //TODO
 
 //fonction de visualisation
 //affiche le labyrinthe et la progression du solveur
 //renvoie -1 en cas d'erreur, 1 sinon
 //maze : le labyrinthe
-int show_shortest_exit_right_hand(maze_t maze); //TODO
+int show_best_exit_deep_seeker(maze_t maze); //TODO
 
 //fonction de visualisation
 //affiche le chemin dans le labyrinthe
