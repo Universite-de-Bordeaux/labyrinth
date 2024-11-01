4e015f4e1bb7c77d5cd67a2d63b63f6c6d7106b6
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-11-01 21:25:08 +0100

    main.c -fin des tests
    Signed-off-by: Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>

--- src/main.c
+++ src/main.c
@@ -2,22 +2,6 @@
 #include "struct.h"
 
 int main(const int argc, char *argv[]) {
-    queue *q = create_queue();
-    for(int i = 0; i < 12; i++)
-    {
-        enqueue(i, i, q);
-        printf("size : %d\n", size_queue(q));
-    }
-    print_queue(q);
-    int x, y;
-    for(int i = 0; i < 9; i++)
-    {
-        dequeue(q, &x, &y);
-        printf("dequeue \nx : %d y : %d\n", x, y);
-        printf("size : %d\n", size_queue(q));
-    }
-    print_queue(q);
-    free_queue(q);
-    //cmd(argv, argc);
+    cmd(argv, argc);
     return 0;
 }
