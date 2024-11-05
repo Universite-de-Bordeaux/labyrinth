2bccd31db032be70a73ec7ce5da33d2ada7f348d
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-11-05 16:40:39 +0100

    struct.c -optimisation -maj purement pour moi
    Signed-off-by: Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>

--- src/struct.c
+++ src/struct.c
@@ -729,93 +729,6 @@ void push(const int x, const int y, stack *p) {
 
 // --- MAZE PRINTING ---
 
-int print_maze(maze_t const maze)
-{
-    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0) //initilisation de la SDL avec l'image et les events (comprends des malloc)
-    {
-        fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError());
-        SDL_Quit();
-        return -1;
-    }
-
-    SDL_DisplayMode displayMode;
-    if (SDL_GetCurrentDisplayMode(0, &displayMode) != 0) { //on obtient le mode d'affichage de l'écran
-        fprintf(stderr, "Erreur lors de l'obtention du mode d'affichage : %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
-    int d_h = displayMode.h/maze.height; int d_w = displayMode.w/maze.width; //on définie le ratio de la taille des cellules
-    d_h = d_h > d_w ? d_w : d_h; //on prend le plus petit ratio pour que le labyrinthe tienne dans l'écran
-    d_w = d_h;
-    if(d_w < 2) //la taille minimale des cellules est de 2 pixels (1 pixel de vide et 2 pixels pour chaque mur)
-    {
-        fprintf(stderr, "Warning : la taille des cellules est trop petite pour être affichée correctement, l'affichage va dépasser de l'écran\n");
-        d_w = 2;
-        d_h = 2;
-    }
-
-    SDL_Window *fenetre = SDL_CreateWindow("maze", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, maze.width * d_w, maze.height * d_h, SDL_WINDOW_SHOWN); //creation d'une fenetre
-    if(fenetre == NULL)
-    {
-        fprintf(stderr, "Erreur de creation de la fenetre : %s\n", SDL_GetError());
-        SDL_Quit();
-        return -1;
-    }
-    SDL_Renderer *renderer = SDL_CreateRenderer(fenetre, -1, SDL_RENDERER_ACCELERATED); //creation d'un renderer
-    if(renderer == NULL)
-    {
-        renderer = SDL_CreateRenderer(fenetre, -1, SDL_RENDERER_SOFTWARE);
-        if(renderer == NULL)
-        {
-            fprintf(stderr, "Erreur de creation du renderer : %s\n", SDL_GetError());
-            SDL_DestroyWindow(fenetre);
-            SDL_Quit();
-            return -1;
-        }
-    }
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); //on définit la couleur de fond en blanc
-
-    for(int x = 0; x < maze.width; x++)
-    {
-        for(int y = 0; y < maze.height; y++)
-        {
-            if(maze.cells[y][x].wall_down)
-            {
-                SDL_RenderDrawLine(renderer, x * d_w, (y + 1) * d_h - 1, (x + 1) * d_w - 1, (y + 1) * d_h - 1); //on dessine un mur en bas
-            }
-            if(maze.cells[y][x].wall_right)
-            {
-                SDL_RenderDrawLine(renderer, (x + 1) * d_w - 1, y * d_h, (x + 1) * d_w - 1, (y + 1) * d_h - 1); //on dessine un mur à droite
-            }
-        }
-    }
-    SDL_RenderDrawLine(renderer, 0, 0, 0, maze.height * d_h); //on dessine les murs de la bordure gauche
-    SDL_RenderDrawLine(renderer, 0, 0, maze.width * d_w, 0); //on dessine les murs de la bordure haute
-
-    SDL_SetRenderDrawColor(renderer, 0, 50, 255, 255); //on définit la couleur en bleu
-    SDL_RenderDrawLine(renderer, 0, 0, d_w, 0); //on dessine l'entrée
-    SDL_RenderDrawLine(renderer, 0, 0, 0, d_h); //on dessine l'entrée
-
-    SDL_SetRenderDrawColor(renderer, 10, 235, 10, 255); //on définit la couleur en vert
-    SDL_RenderDrawLine(renderer, maze.width * d_w - d_w, maze.height * d_h - 1, maze.width * d_w, maze.height * d_h - 1); //on dessine la sortie
-    SDL_RenderDrawLine(renderer, maze.width * d_w - 1, maze.height * d_h - d_h, maze.width * d_w - 1, maze.height * d_h); //on dessine la sortie
-
-    SDL_Delay(displayMode.refresh_rate); //pause pour laisser aux données le temps de s'afficher
-    SDL_RenderPresent(renderer); //on met à jour l'affichage
-    SDL_Event event = {0}; //on crée un event vide
-    while(!(event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE || \
-        (event.type == SDL_KEYUP && (event.key.keysym.sym == SDLK_ESCAPE || \
-            event.key.keysym.sym == SDLK_KP_ENTER || event.key.keysym.sym == SDLK_RETURN)))) //tant que l'utilisateur n'a pas fermé la fenetre
-    {
-        SDL_WaitEvent(&event); //on enregistre les events entrants
-    }
-    SDL_DestroyRenderer(renderer); //destruction du renderer (desallocation de la memoire)
-    SDL_DestroyWindow(fenetre); //destruction de la fenetre (desallocation de la memoire)
-    SDL_Quit(); //desalocation de la memoire
-    return 1;
-}
-
 int initial_print_maze(const maze_t maze, SDL_Renderer **renderer, SDL_Window **window, int *dw, int *dh)
 {
     if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0) //initilisation de la SDL avec l'image et les events (comprends des malloc)
@@ -832,7 +745,8 @@ int initial_print_maze(const maze_t maze, SDL_Renderer **renderer, SDL_Window **
         return 1;
     }
 
-    int d_h = displayMode.h/maze.height; int d_w = displayMode.w/maze.width; //on définie le ratio de la taille des cellules
+    int d_h = (displayMode.h - 40)/maze.height; int d_w = displayMode.w/maze.width; //on définie le ratio de la taille des cellules
+    //le -40 est pour les 40 pixels du haut de mon écran qui sont occupés par la barre de titre et empêche l'affichage, Vincent
     d_h = d_h - 1 > d_w ? d_w : d_h - 1; //on prend le plus petit ratio pour que le labyrinthe tienne dans l'écran
     d_w = d_h;
     if(d_w < 2) //la taille minimale des cellules est de 2 pixels (1 pixel de vide et 2 pixels pour chaque mur)
@@ -913,4 +827,20 @@ void wait_and_destroy_print_maze(SDL_Renderer *renderer, SDL_Window *window)
         SDL_WaitEvent(&event); //on enregistre les events entrants
     }
     destroy_print_maze(renderer, window);
+}
+
+int print_maze(const maze_t maze)
+{
+    SDL_Renderer **renderer = NULL;
+    SDL_Window **window = NULL;
+    int *dw = 0;
+    int *dh = 0;
+    if(initial_print_maze(maze, renderer, window, dw, dh) == -1)
+    {
+        // ReSharper disable twice CppDFANullDereference
+        destroy_print_maze(*renderer, *window);
+        return -1;
+    }
+    wait_and_destroy_print_maze(*renderer, *window);
+    return 1;
 }
