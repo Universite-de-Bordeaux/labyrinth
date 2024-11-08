a02dbb076a7f4327a70d42865b39deb9b3bf0e8e
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-11-09 00:01:09 +0100

    main.c -fin tests
    Signed-off-by: Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>

--- src/main.c
+++ src/main.c
@@ -1,32 +1,9 @@
-#include "cmd.h"
-#include "mazemaker.h"
-#include "struct.h"
-#include "solveur.h"
+#include <stdlib.h>
 #include <time.h>
-
-#include "outside.h"
+#include "cmd.h"
 
 int main(const int argc, char *argv[]) {
     srand(time(NULL));
-    const maze_t maze = hunt_kill_maze(200, 5);
-    for(int i = 0; i < 100; i++)
-    {
-        for(int j = 0; j < 100; j++)
-        {
-            const int t = rand() % 6;
-            if(t == 0)
-            {
-                unwall_down(maze, j, i);
-            }
-            else if(t == 1)
-            {
-                unwall_right(maze, j, i);
-            }
-        }
-    }
-    show_best_exit_deep_seeker(maze);
-    maze_to_file(maze, "test");
-    free_maze(maze);
-    //cmd(argv, argc);
+    cmd(argv, argc);
     return 0;
 }
