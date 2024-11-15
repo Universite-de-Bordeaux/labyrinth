e2f5d20a797c792462ed93b6ce70cfe5a82e776f
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-11-15 13:17:18 +0100

    test.h test.c -transformation evaluate_time (MACRO => fonction)
    Signed-off-by: Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>

--- src/test.h
+++ src/test.h
@@ -53,72 +53,9 @@ int evaluate_mazemaker(func_ptr f);
         } \
     } while(0)
 
-// MACRO to evaluate the time of a maze generator
+// function to evaluate the time of a maze generator
 //générateur : pointer to the maze generator function
 //name : name of the generator
 //print a score between 0 (bad) and 100 (good) of the generator
-#define EVALUATE_TIME(générateur, name) \
-    do { \
-        int score1 = 50; \
-        int score2 = 10; \
-        int score3 = 15; \
-        int score4 = 25; \
-        clock_t start = clock(); \
-        for(int i = 1; i < 100; i++) \
-        { \
-            for(int j = 1; j < 100; j++) \
-            { \
-                const maze_t maze = générateur(i, j); \
-                free_maze(maze); \
-            } \
-            if(clock() - start > 200000000) \
-            { \
-            score1 = 0; \
-            break; \
-            } \
-        } \
-        score1 -= (clock() - start) / 4000000; \
-        clock_t inter = clock(); \
-        for(int k = 0; k < 1000; k++) \
-        { \
-            for(int i = 1; i < 100; i++) \
-            { \
-                const maze_t maze = générateur(i, 1); \
-                free_maze(maze); \
-            } \
-            const maze_t maze = générateur(1, k); \
-            free_maze(maze); \
-        } \
-        score2 -= (clock() - inter) / 35000; \
-        clock_t inter2 = clock(); \
-        for(int k = 0; k < 1000; k++) \
-        { \
-            const maze_t maze = générateur(4, 4); \
-            free_maze(maze); \
-        } \
-        score3 -= (clock() - inter2) / 400; \
-        for(int k = 0; k < 10; k++) \
-        { \
-            const maze_t maze = générateur(100, 100); \
-            free_maze(maze); \
-        } \
-        score4 -= (clock() - inter2) / 100000; \
-        if(score1 < 0) \
-        { \
-            score1 = 0; \
-        } \
-        if(score2 < 0) \
-        { \
-            score2 = 0; \
-        } \
-        if(score3 < 0) \
-        { \
-            score3 = 0; \
-        } \
-        if(score4 < 0) \
-        { \
-            score4 = 0; \
-        } \
-        printf("Le générateur %s a un score de %d (%d %d %d %d)\n", name, score1 + score2 + score3 + score4, score1, score2, score3, score4); \
-    } while(0)
+int evaluate_time(func_ptr f, char* c);
 #endif //TEST_H
