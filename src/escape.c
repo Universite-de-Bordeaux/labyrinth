55663024f7d7bb4cb802b987671008516ef8e2ed
Micky Mari <micky.mari@etu.u-bordeaux.fr>
2025-02-09 08:34:24 +0100

    Revert "fix escape.c"
    This reverts commit 0e2e39e48b3bbc528ac28af19415167206954ae7.

--- src/escape.c
+++ src/escape.c
@@ -1038,7 +1038,7 @@ int right_hand_random(const maze_t maze, int x, int y)
                 {
                     dir = rand() % 4;
                 }
-                while (!(can_go(x, y, maze, dir) && visited_value(visited, x, y, dir) == min)); // on choisit une des cellules avec le moins de passages
+                while ((visited_value(visited, x, y, dir) != min) || !can_go(x, y, maze, dir)); // on choisit une des cellules avec le moins de passages
             }
         }
         else
