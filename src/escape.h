26c029dcd47a6fdd40f099cc86cb24c5e9e1817e
Anna Mielcarek <anna.mielcarek79@gmail.com>
2025-02-13 16:29:18 +0100

    escape.h suppression de la fonction type

--- src/escape.h
+++ src/escape.h
@@ -6,14 +6,6 @@
 extern int (*escape[SIZE])(maze_t maze, int x, int y);
 extern const char *escape_name[SIZE];
 
-// standard function to find the exit of the maze
-// visualize the progression of the solver
-// stop when the exit is found or the corresponding command is entered
-// maze : the maze
-// x, y : the coordinates of the starting point
-// return the number of steps
-int ESCAPE_TYPE(maze_t maze, int x, int y);
-
 // function to find the exit of the maze from an random case
 // visualize the progression of the solver
 // stop when the exit is found or the corresponding command is entered
