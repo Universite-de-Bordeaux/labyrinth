8f6089e107816e0edb939226c99b4c23a6cc9710
Micky Mari <micky.mari@etu.u-bordeaux.fr>
2025-02-20 15:22:17 +0100

    updt escape.c, escape.h, Rapport Interne.md - Complétion du rapport interne - Supression des escapes random_p, random_de, right_hand_p, right_hand_de - Supression de ces escapes dans les tableaux escape et escape_names, et de la déclaration du .h

--- src/escape.h
+++ src/escape.h
@@ -47,11 +47,19 @@ int right_hand(maze_t maze, int x, int y);
 int right_hand_r(maze_t maze, int x, int y);
 
 
-int right_hand_p(maze_t maze, int x, int y);
-int right_hand_de(const maze_t maze, int x, int y);
+// function to find the exit of the maze using the right hand among the less visited cells which are not detected dead ends
+// visualize the progression of the solver
+// stop when the exit is found or the corresponding command is entered
+// maze : the maze
+// x, y : the coordinates of the starting point
+// return the number of steps
 int right_hand_p_de(const maze_t maze, int x, int y);
 
-int random_p(const maze_t maze, int x, int y);
-int random_de(const maze_t maze, int x, int y);
+// function to find the exit of the maze using random movement among the less visited cells which are not detected dead ends
+// visualize the progression of the solver
+// stop when the exit is found or the corresponding command is entered
+// maze : the maze
+// x, y : the coordinates of the starting point
+// return the number of steps
 int random_p_de(const maze_t maze, int x, int y);
 #endif // ESCAPE_H
